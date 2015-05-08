64c0a152798f23450ce8ca80f6fb6f8e84e94ab4
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-08 16:05:38 +0200

    renamed test

--- src/test/java/com/sepoe/wbitdd/pos/PointOfSaleCreationTest.java
+++ src/test/java/com/sepoe/wbitdd/pos/PointOfSaleCreationTest.java
@@ -1,4 +1,5 @@
 package com.sepoe.wbitdd.pos;
+
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -10,7 +11,7 @@ import static org.junit.Assert.fail;
  *
  * @author L.Hankewitz
  * @since 08/05/15.
- *
+ * <p>
  * Testlist:
  * ToTest: mandatory item repo and output device
  * ToTest: lookup of barcode
@@ -19,7 +20,7 @@ import static org.junit.Assert.fail;
  * ToTest: output invalid barcode
  * ToTest: output of not found item for barcode
  */
-public class PointOfSaleTest {
+public class PointOfSaleCreationTest {
     @Test
     public void creatingPointOfSale_withMissingOutputDevice_throwsException() {
         try {
@@ -37,5 +38,7 @@ public class PointOfSaleTest {
             fail("Creation of PointOfSale without item repository should throw exception");
         } catch (IllegalArgumentException e) {
             assertThat(e.getMessage(), containsString("Missing item repository"));
-        }    }
+        }
+    }
+
 }
