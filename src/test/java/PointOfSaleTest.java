6371d6c603e007ccd8867f68293a9c7a2e5fbe8e
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-08 14:16:45 +0200

    Experiment with ToTest todo items in intellij for the test list.

---
+++ src/test/java/PointOfSaleTest.java
@@ -0,0 +1,12 @@
+/**
+ * Class to test the point of sale component of the WBITTD of J.B. Rainsberger.
+ *
+ * @author L.Hankewitz
+ * @since 08/05/15.
+ *
+ * Testlist:
+ * ToTest:
+ */
+public class PointOfSaleTest {
+
+}
