bce0eef780a5e714172649eae4e24f3e78b8ea3a
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-15 19:06:38 +0200

    remember test for two subsequent sales

---
+++ src/test/java/com/sepoe/wbitdd/pos/HandleTwoSubsequentSales.java
@@ -0,0 +1,18 @@
+package com.sepoe.wbitdd.pos;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Class to test to subsequent tests.
+ * @author lumiha
+ * @since 15/05/15.
+ */
+public class HandleTwoSubsequentSales {
+
+    // ToTest: subsequent sales
+    @Test
+    @Ignore("Handling reset of total after sale")
+    public void onTotal_forTwoSubsequentSales_calculatesTwoTotals() {
+    }
+}
