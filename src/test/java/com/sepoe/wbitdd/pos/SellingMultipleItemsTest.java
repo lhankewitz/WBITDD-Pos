3f1895f92a0064f1ebc2c9700f7332aeb6a41c5e
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-15 18:48:14 +0200

    Ignore multiple items test with prices

--- src/test/java/com/sepoe/wbitdd/pos/SellingMultipleItemsTest.java
+++ src/test/java/com/sepoe/wbitdd/pos/SellingMultipleItemsTest.java
@@ -1,5 +1,6 @@
 package com.sepoe.wbitdd.pos;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -12,9 +13,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * @since 15/05/15.
  */
 public class SellingMultipleItemsTest {
+    private static final int BARCODE_IDX = 0;
+    private static final int PRICE_IDX = 1;
     private MockOutputDevice display = new MockOutputDevice();
     private final MockItemRepository mockItemRepository = new MockItemRepository();
     private PointOfSale pos = new PointOfSale(mockItemRepository, display);
+
+    private BarcodeGenerator barcodeGenerator = new BarcodeGenerator();
 
     @Test
     public void onTotal_forNoItmems_returnsMessage() {
@@ -29,5 +34,39 @@ public class SellingMultipleItemsTest {
         pos.onBarcode(barcode);
         pos.onTotal();
         assertThat(display.getOutputToWrite(), is("Total $8.50"));
+    }
+
+    @Test
+    @Ignore("requires refactoring first in POS")
+    public void onTotal_forThreeFoundItems_resultsInTotal() {
+        final String[] barcodes = generateBarcodes();
+
+        register(barcodes);
+
+        scan(barcodes);
+
+        pos.onTotal();
+
+        assertThat(display.getOutputToWrite(), is("Total $24.55"));
+    }
+
+    private String[] generateBarcodes() {
+        return new String[]{
+                    barcodeGenerator.generateBarCode()
+                    ,barcodeGenerator.generateBarCode()
+                    ,barcodeGenerator.generateBarCode()
+            };
+    }
+
+    private void register(final String[] barcodes) {
+        mockItemRepository.when(barcodes[0], 8.50);
+        mockItemRepository.when(barcodes[1], 12.75);
+        mockItemRepository.when(barcodes[2], 3.30);
+    }
+
+    private void scan(final String[] barcodes) {
+        for (String barcode : barcodes) {
+            pos.onBarcode(barcode);
+        }
     }
 }
